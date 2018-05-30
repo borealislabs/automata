4c48f7e4d53bb21de49af5fa9cb7844018e2881c
James Shane <jamesshane@gmail.com>
2018-05-30 06:35:25 +0000

    working on james way...

---
+++ james_way1.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt-get update
+
+sudo apt-get install php5.6 php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-mbstring php5.6-sqlite3 apache2 libapache2-mod-php5.6
+
